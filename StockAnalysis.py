3ea7f58020009659662dd758784c792aaa22e4eb
akalia25 <32119652+akalia25@users.noreply.github.com>
2020-01-14 01:48:38 -0500

    appraisal using moving average
    now conducts stocks appraisal rating using the stocks moving average closing prices on a 5,15,30 day period to determine optimal position with the stock

--- StockAnalysis.py
+++ StockAnalysis.py
@@ -73,6 +73,21 @@ def zvalue(series):
 
 
 def stock_appraisal_z_value(stocks_df):
+    """
+    The purpose of this function is to consider the z-value of each stocks
+    closing prices and determine what the optimal position for that stock
+    is (Hold, Buy, Sell)
+    Parameters
+    ----------
+    stocks_df : Pandas DataFrame
+    Contains the pandas DataFrame with all the stocks entered by the user and
+    their trading history (close price, volume, date, etc)
+    Returns
+    -------
+    stock_appraisal_z_value_df : Pandas DataFrame
+    Uses the stock as the index and the other columns represent the optimal
+    position of the stock
+    """
     stock_zval = {}
     stock_appraisal = {}
     for stock in stocks_df.StockName.unique():
@@ -94,18 +109,50 @@ def stock_appraisal_z_value(stocks_df):
 
 
 def stock_appraisal_moving_average(stocks_df):
+    """
+
+    The purpose of this function is to use the stocks closing price to create
+    its moving average, and trigger the optimal postion of the stock based on
+    how the stocks moving average is progressing
+
+    Parameters
+    ----------
+    stocks_df : Pandas DataFrame
+    Contains the pandas DataFrame with all the stocks entered by the user and
+    their trading history (close price, volume, date, etc)
+    Returns
+    -------
+    stock_appraisal_moving_average : Pandas DataFrame
+    """
+    stock_appraisal = {}
     for stock in stocks_df.StockName.unique():
-        df1 = stocks_df['Close'][(stocks_df.StockName == stock)][-30:]
-        for moving_average_period in (5, 15):
-            moving_average = df1.rolling(window=moving_average_period).mean()
+        df1 = stocks_df[['Close',
+                         'StockName']][(stocks_df.StockName == stock)][-30:]
+        data = stocks_df['Close'][(stocks_df.StockName == stock)][-30:]
+        for moving_average_period in (5, 15, 30):
+            moving_average = data.rolling(window=moving_average_period).mean()
             df1.loc[:, 'SMA ' + str(stock) + ' ' + str(
                     moving_average_period)] = moving_average.values
+        final_series = df1.iloc[-1, [-1, -2, -3]] - df1.iloc[-1, 0]
+        if final_series[0] > 0 and final_series[1] > 0 and final_series[2] > 0:
+            stock_appraisal[stock] = 'BUY'
+        elif final_series[0] < 0 and final_series[1] < 0 and final_series[2] < 0:
+            stock_appraisal[stock] = 'SELL'
+        else:
+            stock_appraisal[stock] = 'HOLD'
+    print(stock_appraisal)
+    stock_appraisal_moving_average_df = pd.DataFrame(list(stock_appraisal.values()),
+                                  columns=['moving_average_appraisal'],
+                                  index=stock_appraisal.keys())
+    return stock_appraisal_moving_average_df
+
 
 def main():
     stocks = user_input().split(',')
     stocks = cleanse_stocks(stocks)
     stocks_df = historical_data(stocks)
     stock_appraisal_z_value(stocks_df)
+    stock_appraisal_moving_average(stocks_df)
 
 
 if __name__ == '__main__':
